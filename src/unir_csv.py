6e8d36730f7839d90d0725079e87aade0317ca1c
Imanol Duran Tejada <imanol.duran@opendeusto.es>
2024-03-26 00:22:54 +0100

    Create unir_csv.py

---
+++ src/unir_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import glob
+
+# Lista todos los archivos CSV en el directorio actual
+archivos_csv = glob.glob("*.csv")
+
+# Leer el primer archivo CSV para obtener la cabecera
+primer_csv = pd.read_csv(archivos_csv[0])
+
+# Leer los otros archivos CSV y unirlos en uno solo
+datos_unidos = pd.concat([pd.read_csv(f) for f in archivos_csv])
+
+# Escribir los datos unidos en un nuevo archivo CSV en el directorio actual
+datos_unidos.to_csv("datos_unidos.csv", index=False)
+
+print("Los archivos CSV se han unido exitosamente en 'datos_unidos.csv'")
